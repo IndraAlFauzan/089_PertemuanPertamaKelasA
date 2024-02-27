67e8832d3b868ae2a6674e7fb8cb72a659d7711c
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2024-02-27 18:49:50 +0700

    Add project files.

---
+++ PertemuanPertamaKelasA/PertemuanPertamaKelasA.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+
+		// numeric nJejari, nLuas
+		// display 'Masukan jejari= '
+		// accept nJejari
+		// compute nLuas = 3.14 * r * r
+		// display 'Luasnya= ' + nLuas
+	float nJejari, nLuas;
+	cout << "Masukan jejari = ";
+	cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "Luasnya =" << nLuas << endl;
+
+
+	system("pause");
+}//end
